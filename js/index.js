1ad2aa448f77158ddffce2f27642c6a15092cc09
BruhdyBro <broudynegron@gmail.com>
2026-05-26 17:49:57 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -21,9 +21,129 @@ const skills = ["JavaScript", "HTML", "CSS", "C", "Java", "GDScript", "VSCode", 
 const skillsSection = document.getElementById("skills");
 var skillsList = skillsSection.querySelector("ul");
 
-for (let mySkill in skills) {
+for (let mySkill of skills) {
 
     var skill = document.createElement("li");
-    skill.innerHTML = (skills[mySkill]);
+    skill.innerHTML = (mySkill);
     skillsList.appendChild(skill);
 }
+
+//Messaging Form System
+const messageForm = document.getElementsByName("leave_message")[0];
+messageForm.addEventListener("submit", function(event) {
+
+    // Remove default events (i.e page refresh)
+    event.preventDefault();
+
+    // Get user's name, email, and message
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    // Log to console for testing purposes
+    console.log(usersName);
+    console.log(usersEmail);
+    console.log(usersMessage);
+    
+    // Adding User's message to Message Board
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+
+    // Create list item with user's name and link to their email, along with message
+    const newMessage = document.createElement("li"); 
+    
+    // Email and Name Link
+    const emailName = document.createElement("a")
+    emailName.className = "email-name";
+    emailName.href = `mailto:${usersEmail}`;
+    emailName.textContent = usersName;
+
+    // Span for the message
+    const messageSpan = document.createElement("span");
+    messageSpan.className = "span-text";
+    messageSpan.textContent = usersMessage;
+
+    newMessage.appendChild(emailName);
+    newMessage.appendChild(messageSpan);
+
+    // Create a remove button that removes specific message
+    const removeButton = document.createElement("button");
+    removeButton.className = "remove-button";
+    removeButton.innerHTML = "Remove";
+    removeButton.type = "button";
+
+    // On Remove button click
+    removeButton.addEventListener("click", function() {
+
+        //remove parent (whole message) node
+        const entry = this.parentNode;
+        entry.remove();
+    });
+
+    // Create an edit button
+    const editButton = document.createElement("button");
+    editButton.className = "edit-button";
+    editButton.innerHTML = "Edit";
+    editButton.type = "button";
+
+    // On edit button click
+    editButton.addEventListener("click", function() {
+
+        // Does not create if edit box already exists
+        if (newMessage.querySelector("textarea")) {
+
+            return;
+        }
+
+        // Create new text area for user to type in
+        const editText = document.createElement("textarea");
+        editText.className = "edit-textarea";
+        editText.innerHTML = messageSpan.textContent;
+
+        // Create confirm changes button
+        const confirmChangesButton = document.createElement("button");
+        confirmChangesButton.className = "confirm-button"
+        confirmChangesButton.innerHTML = "Confirm";
+        confirmChangesButton.type = "button";
+
+        // Create cancel changes button
+        const cancelChangesButton = document.createElement("button");
+        cancelChangesButton.className = "cancel-button";
+        cancelChangesButton.innerHTML = "Cancel";
+        cancelChangesButton.type = "button";
+
+        // On confirm changes button click
+        confirmChangesButton.addEventListener("click", function() {
+
+            
+            messageSpan.textContent = editText.value;
+            this.remove();
+            editText.remove();
+            cancelChangesButton.remove();
+        })
+
+        // On cancel changes button click
+        cancelChangesButton.addEventListener("click", function() {
+
+            this.remove();
+            editText.remove();
+            confirmChangesButton.remove()
+        })
+
+        
+        // Append Editing stuff to Message
+        this.parentNode.appendChild(editText);
+        this.parentNode.appendChild(confirmChangesButton);
+        this.parentNode.appendChild(cancelChangesButton);
+
+    });
+
+    // Append all the buttons and stuff to the message
+    newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);
+    
+    messageList.appendChild(newMessage);
+
+    // Empties the form 
+    messageForm.reset();
+});
